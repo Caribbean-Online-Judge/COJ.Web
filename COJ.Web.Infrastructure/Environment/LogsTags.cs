18a2179d9ad154b118cbd0a1e2cd00889e57ad1c
Arael D. Espinosa Pérez <cl8dep@gmail.com>
2022-01-09 16:46:34 -0500

    Minor refactors

--- COJ.Web.Infrastructure/Environment/LogsTags.cs
+++ COJ.Web.Infrastructure/Environment/LogsTags.cs
@@ -1,5 +1,7 @@
 namespace COJ.Web.Infrastructure.Environment;
 
-public class LogsTags {
+public class LogsTags
+{
     public const string EMAIL_SERVICE_FATAL_ERROR = "Email service fatal error";
+    internal static readonly string SignIn = "Sign in ";
 }
